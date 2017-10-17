0c9d78f693fc9ab182b5dadbf1a048dfa4079bf5
Nick Edwards <nledwa01@gmail.com>
2017-10-16 21:09:11 -0400

    Set up Log In form and ability to authenticate a Username and Password.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,31 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+// GET /login
+router.get('/login', function(req, res, next) {
+  return res.render('login', {title: 'Log In'});
+});
+
+// POST /login
+router.post('/login', function(req, res, next){
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userID = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error ('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
 // GET /register
 router.get('/register', function(req, res, next) {
   return res.render('register', {title: 'Sign Up'});
@@ -35,6 +60,7 @@ router.post('/register', function(req, res, next) {
         if (error) {
           return next(error);
         } else {
+          req.session.userID = user._id;
           return res.redirect('/profile');
         }
       });
